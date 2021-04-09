743fddde4c640b98d23f04023b70387923a66e2d
Adam <adamo.b1997@gmail.com>
2021-04-09 10:30:39 +0200

    Update FlightController.cs

--- Airport.Api/Controllers/FlightController.cs
+++ Airport.Api/Controllers/FlightController.cs
@@ -1,8 +1,11 @@
-﻿using System.Net;
+﻿using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Airport.Domain.DTOs;
 using Airport.Domain.Models;
+using Airport.Infrastructure.Helpers.PaginationParameters;
 using Airport.Infrastructure.Interfaces;
+using AirportBackend.Extensions;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +37,23 @@ namespace AirportBackend.Controllers
             var result = await _flightRepository.Add(flight);
 
             return result ? Ok() : StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+        
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int) HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetFlights([FromQuery] FlightParameters parameters)
+        {
+            var flights = await _flightRepository.GetFlights(parameters);
+
+            if (flights == null || !flights.Any()) return BadRequest("Could not find any flights");
+
+            Response.AddPaginationHeader(flights.CurrentPage, flights.PageSize, flights.TotalCount, flights.TotalPages);
+
+            return Ok(flights);
         }
     }
 }
