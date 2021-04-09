b20f10d80eb17a0569dfcc6efe905e36b78d3f09
Adam <adamo.b1997@gmail.com>
2021-04-09 12:56:05 +0200

    Add AirportController + AirportRepository methods

--- Airport.Api/Controllers/AirportController.cs
+++ Airport.Api/Controllers/AirportController.cs
@@ -9,17 +9,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AirportBackend.Controllers
 {
-    [AllowAnonymous]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AirportController : ControllerBase
     {
-        private readonly IAirportRepository _airportEntityRepository;
+        private readonly IAirportRepository _airportRepository;
         private readonly IMapper _mapper;
 
         public AirportController(IAirportRepository airportRepository, IMapper mapper)
         {
-            _airportEntityRepository = airportRepository;
+            _airportRepository = airportRepository;
             _mapper = mapper;
         }
 
@@ -31,9 +31,26 @@ namespace AirportBackend.Controllers
         {
             var airport = _mapper.Map<AirportEntity>(airportForAdd);
 
-            var result = await _airportEntityRepository.Add(airport);
+            var result = await _airportRepository.Add(airport);
 
             return result ? Ok() : StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        [HttpGet("count")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(int), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int) HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int) HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetNumberOfAirports()
+        {
+            var result = await _airportRepository.GetNumberOfAirports();
+
+            return result switch
+            {
+                < 0 => StatusCode((int) HttpStatusCode.InternalServerError),
+                0 => BadRequest("Could not find any airports"),
+                _ => Ok(result)
+            };
         }
     }
 }
