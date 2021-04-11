e9e1b98e24eb7ee3f217fa8297c0ad2185ddb879
Adam <adamo.b1997@gmail.com>
2021-04-11 09:14:41 +0200

    Add Payment model

---
+++ Airport.Domain/Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace Airport.Domain.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string ReferenceNumber { get; set; }
+        public bool IsPaid { get; set; }
+        public int BookingId { get; set; }
+        public Booking Booking { get; set; }
+    }
+}
