d057204baf1f1e87b8b985af11c8631219d908ac
Adam <adamo.b1997@gmail.com>
2021-04-12 14:44:26 +0200

    Update and Fix models

--- Airport.Domain/Models/Airplane.cs
+++ Airport.Domain/Models/Airplane.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Airport.Domain.Models
 {
@@ -16,6 +12,7 @@ namespace Airport.Domain.Models
         public bool IsInRepair { get; set; }
         public int LocationId { get; set; }      
         public AirportEntity Location { get; set; }
+        public int NumberOfSeats { get; set; }
 
         public virtual IEnumerable<Flight> Flights { get; set; }
     }
