a4a4cee1aa9f1621292432df2ae683311e5d254a
Adam <adamo.b1997@gmail.com>
2021-04-19 22:04:44 +0200

    Add Migration

---
+++ Airport.Infrastructure/Migrations/20210419195921_BookingAcceptance.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Airport.Infrastructure.Migrations
+{
+    public partial class BookingAcceptance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "Bookings");
+        }
+    }
+}
