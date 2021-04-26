4c39dce060982aaa7fef87f2d5b3874783fd7f85
Adam <adamo.b1997@gmail.com>
2021-04-26 15:06:43 +0200

    Refactor Models

---
+++ Airport.Domain/Models/PassengerBooking.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Airport.Domain.Models
+{
+    public class PassengerBooking
+    {
+        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public int BookingId { get; set; }
+        public Passenger Passenger { get; set; }
+        public Booking Booking { get; set; }
+    }
+}
