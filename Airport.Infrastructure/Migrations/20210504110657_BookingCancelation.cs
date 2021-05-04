728ca4eaf7defe80405bf6c2e54ed546366a920f
matzaj1 <matzaj312@gmail.com>
2021-05-04 13:32:04 +0200

    Add migrations

---
+++ Airport.Infrastructure/Migrations/20210504110657_BookingCancelation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Airport.Infrastructure.Migrations
+{
+    public partial class BookingCancelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsAccepted",
+                table: "Bookings",
+                newName: "IsCancelled");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsCancelled",
+                table: "Bookings",
+                newName: "IsAccepted");
+        }
+    }
+}
