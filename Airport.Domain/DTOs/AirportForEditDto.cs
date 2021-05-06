7c5038769b6c213165b7c2302d676e63d760b4a1
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-06 18:39:02 +0200

    Create AirportForEditDto.cs

---
+++ Airport.Domain/DTOs/AirportForEditDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Airport.Domain.DTOs
+{
+    public class AirportForEditDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string CodeIATA { get; set; }
+    }
+}
