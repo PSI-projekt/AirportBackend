ccc34302db4dad6b7c4d4423db531f476110268c
matzaj1 <matzaj312@gmail.com>
2021-05-11 21:20:51 +0200

    Create BookingForListDto.cs

---
+++ Airport.Domain/DTOs/BookingForListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Airport.Domain.DTOs
+{
+    public class BookingForListDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateOfBooking { get; set; }
+        public bool IsCancelled { get; set; }
+        public int NumberOfPassengers { get; set; }
+        public FlightDetailsDto FlightDetails { get; set; }
+        public IEnumerable<PassengerForEditDto> Passengers { get; set; }
+    }
+}
