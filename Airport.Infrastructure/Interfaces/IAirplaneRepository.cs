04d44845caac9cef9773849cb0d5c4a4ed0d8455
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-13 21:47:07 +0200

    Update IAirplaneRepository.cs

--- Airport.Infrastructure/Interfaces/IAirplaneRepository.cs
+++ Airport.Infrastructure/Interfaces/IAirplaneRepository.cs
@@ -10,6 +10,7 @@ namespace Airport.Infrastructure.Interfaces
         Task<bool> Add(Airplane airplane);
         Task<int> GetNumberOfSeatsForFlight(int flightId);
         Task<List<AirplaneForListDto>> GetAirplanes();
-
+        Task<bool> Edit(AirplaneForEditDto airplaneForEdit);
+        Task<bool> Delete(int airplaneId);
     }
 }
