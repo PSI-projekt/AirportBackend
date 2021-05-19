d5b245b1d0361d236512fbb2909bd23f578569c1
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-19 16:54:20 +0200

    Create UserForEditDto.cs

---
+++ Airport.Domain/DTOs/UserForEditDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Airport.Domain.DTOs
+{
+    public class UserForEditDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string StreetNumber { get; set; }
+        [Required]
+        public string PostCode { get; set; }
+        [Required]
+        public string IDNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "You must specify password between 8 and 40 characters")]
+        public string Password { get; set; }
+    }
+}
