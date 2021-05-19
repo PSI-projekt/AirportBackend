fbdd3d36ffc704fe5b3d9be9b71bb3433fbbce28
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-19 16:54:01 +0200

    Update IUserRepository.cs

--- Airport.Infrastructure/Interfaces/IUserRepository.cs
+++ Airport.Infrastructure/Interfaces/IUserRepository.cs
@@ -6,5 +6,7 @@ namespace Airport.Infrastructure.Interfaces
     public interface IUserRepository
     {
         Task<User> GetUserById(int userId);
+        Task<bool> Edit(UserForEditDto userForEdit);
+        Task<bool> Delete(int userId);
     }
 }
