2b0d306d9f3b711be87787610573bd58798a6960
blazejzaj <62843011+blazejzaj@users.noreply.github.com>
2021-05-27 19:17:46 +0200

    Update AirportForListDto.cs

--- Airport.Domain/DTOs/AirportForListDto.cs
+++ Airport.Domain/DTOs/AirportForListDto.cs
@@ -2,6 +2,7 @@
 {
     public class AirportForListDto
     {
+		public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
